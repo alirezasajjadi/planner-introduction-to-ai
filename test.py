21a1b49155b4afd3b7d18d39eb6d7ea6e4246e66
alirezasajjadi <alirezasajjadi.asj@gmail.com>
2023-05-28 21:59:26 +0330

    modified test.py to solve the instance of BlockWorldProblem

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-from Domains.LinkRepeatDomain import LinkRepeatDomain
-from Problems.LinkRepeatProblem import LinkRepeatProblem
+from Domains.TireDomain import TireDomain
+from Domains.BlocksWorldDomain import BlocksWorldDomain
+from Problems.TireProblem import TireProblem
+from Problems.BlocksWorldProblem import BlocksWorldProblem
+
 from Planners.BackwardPlanner import BackwardPlanner
 
 # Create a BackwardPlanner object with a LinkRepeatProblem instantiated with a LinkRepeatDomain of size 1000
-back = BackwardPlanner(LinkRepeatProblem(LinkRepeatDomain(1000)))
+back = BackwardPlanner(BlocksWorldProblem(BlocksWorldDomain()))
 
 # Perform a search using the backward planner on the LinkRepeatProblem
 print(back.search())
